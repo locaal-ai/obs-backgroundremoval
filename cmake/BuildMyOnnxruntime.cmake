f9f2f177d08e7c4b821ef3a979d237ff2b448642
Kaito Udagawa <umireon@gmail.com>
2023-04-02 15:01:31 +0900

    Update BuildMyOnnxruntime.cmake

--- cmake/BuildMyOnnxruntime.cmake
+++ cmake/BuildMyOnnxruntime.cmake
@@ -91,7 +91,7 @@ ExternalProject_Add(
   GIT_REPOSITORY https://github.com/microsoft/onnxruntime.git
   GIT_TAG v1.14.1
   GIT_SHALLOW ON
-  PATCH_COMMAND git apply < ${CMAKE_SOURCE_DIR}/cmake/onnxruntime-v1.14.1.patch
+  PATCH_COMMAND git apply ${CMAKE_SOURCE_DIR}/cmake/onnxruntime-v1.14.1.patch || true
   CONFIGURE_COMMAND "${Onnxruntime_PLATFORM_CONFIGURE}"
   BUILD_COMMAND
     ${PYTHON3} <SOURCE_DIR>/tools/ci_build/build.py --build_dir <BINARY_DIR> --config
