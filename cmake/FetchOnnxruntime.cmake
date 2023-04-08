24db10f812132ac30eac3228d5f00ff1226c0f31
Kaito Udagawa <umireon@gmail.com>
2023-04-08 12:17:03 +0900

    Update FetchOnnxruntime.cmake

--- cmake/FetchOnnxruntime.cmake
+++ cmake/FetchOnnxruntime.cmake
@@ -26,20 +26,13 @@ elseif(OS_LINUX)
     URL_HASH MD5=9725836c49deb09fc352a57dc8a1b806)
   FetchContent_MakeAvailable(Onnxruntime)
   set(Onnxruntime_LIBS
-    "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_cuda.so"
-    "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_tensorrt.so"
-    "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_shared.so"
-    "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime.${Onnxruntime_VERSION}.so"
-    )
+      "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_cuda.so"
+      "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_tensorrt.so"
+      "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_shared.so"
+      "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime.${Onnxruntime_VERSION}.so")
   target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE "${Onnxruntime_LIB}")
   target_include_directories(${CMAKE_PROJECT_NAME} SYSTEM
                              PUBLIC "${onnxruntime_SOURCE_DIR}/include")
-  install(FILES "${Onnxruntime_LIBS}" DESTINATION "${CMAKE_PROJECT_NAME}.plugin/Contents/Frameworks")
-  add_custom_command(
-    TARGET "${CMAKE_PROJECT_NAME}"
-    POST_BUILD
-    COMMAND
-      ${CMAKE_INSTALL_NAME_TOOL} -change "@rpath/libonnxruntime.${Onnxruntime_VERSION}.dylib"
-      "@loader_path/../Frameworks/libonnxruntime.${Onnxruntime_VERSION}.dylib"
-      $<TARGET_FILE:${CMAKE_PROJECT_NAME}>)
+  install(FILES "${Onnxruntime_LIBS}"
+          DESTINATION ".")
 endif()
