6fcea9dfac9b954710444c82b0efbbee6c96db10
Kaito Udagawa <umireon@gmail.com>
2023-04-12 13:47:09 +0900

    Link libs

--- cmake/FetchOnnxruntime.cmake
+++ cmake/FetchOnnxruntime.cmake
@@ -45,5 +45,8 @@ elseif(OS_LINUX)
   FetchContent_MakeAvailable(Onnxruntime)
   set(Onnxruntime_LINK_LIBS "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_shared.so" "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime.so.${Onnxruntime_VERSION}")
   set(Onnxruntime_INSTALL_LIBS "${Onnxruntime_LINK_LIBS}" "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_cuda.so" "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime_providers_tensorrt.so")
-
+  target_link_libraries(${CMAKE_PROJECT_NAME} SYSTEM PUBLIC "${Onnxruntime_LINK_LIBS}")
+  target_include_directories(${CMAKE_PROJECT_NAME} SYSTEM
+                             PUBLIC "${onnxruntime_SOURCE_DIR}/include")
+  install(FILES "${Onnxruntime_INSTALL_LIBS}" DESTINATION ".")
 endif()
