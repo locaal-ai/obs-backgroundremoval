a415560342ae5c3cf46be808a3e1d2d7306ade95
Roy Shilkrot <roy.shil@gmail.com>
2023-05-08 09:56:27 -0400

    Add GPU to enhance filter (#300)
    * add gpu
    
    * lint
    
    * add list object
    
    * add high pass
    
    * comment
    
    * bump ver

--- src/enhance-filter.cpp
+++ src/enhance-filter.cpp
@@ -44,11 +44,23 @@ static obs_properties_t *filter_properties(void *data)
   obs_property_t *p_model_select =
     obs_properties_add_list(props, "model_select", obs_module_text("EnhancementModel"),
                             OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_STRING);
-
   obs_property_list_add_string(p_model_select, obs_module_text("TBEFN"), MODEL_ENHANCE_TBEFN);
   obs_property_list_add_string(p_model_select, obs_module_text("URETINEX"), MODEL_ENHANCE_URETINEX);
   obs_property_list_add_string(p_model_select, obs_module_text("SGLLIE"), MODEL_ENHANCE_SGLLIE);
   obs_property_list_add_string(p_model_select, obs_module_text("ZERODCE"), MODEL_ENHANCE_ZERODCE);
+  obs_property_t *p_use_gpu = obs_properties_add_list(props, "useGPU",
+                                                      obs_module_text("InferenceDevice"),
+                                                      OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_STRING);
+  obs_property_list_add_string(p_use_gpu, obs_module_text("CPU"), USEGPU_CPU);
+#ifdef __linux__
+  obs_property_list_add_string(p_use_gpu, obs_module_text("GPUTensorRT"), USEGPU_TENSORRT);
+#endif
+#if _WIN32
+  obs_property_list_add_string(p_use_gpu, obs_module_text("GPUDirectML"), USEGPU_DML);
+#endif
+#if defined(__APPLE__)
+  obs_property_list_add_string(p_use_gpu, obs_module_text("CoreML"), USEGPU_COREML);
+#endif
   return props;
 }
 
@@ -57,6 +69,14 @@ static void filter_defaults(obs_data_t *settings)
   obs_data_set_default_double(settings, "blend", 1.0);
   obs_data_set_default_int(settings, "numThreads", 1);
   obs_data_set_default_string(settings, "model_select", MODEL_ENHANCE_TBEFN);
+#if _WIN32
+  obs_data_set_default_string(settings, "useGPU", USEGPU_DML);
+#elif defined(__APPLE__)
+  obs_data_set_default_string(settings, "useGPU", USEGPU_CPU);
+#else
+  // Linux
+  obs_data_set_default_string(settings, "useGPU", USEGPU_CPU);
+#endif
 }
 
 static void filter_activate(void *data)
@@ -79,8 +99,9 @@ static void filter_update(void *data, obs_data_t *settings)
   tf->blendFactor = (float)obs_data_get_double(settings, "blend");
   const uint32_t newNumThreads = (uint32_t)obs_data_get_int(settings, "numThreads");
   const std::string newModel = obs_data_get_string(settings, "model_select");
+  const std::string newUseGpu = obs_data_get_string(settings, "useGPU");
 
-  if (tf->modelSelection.empty() || tf->modelSelection != newModel ||
+  if (tf->modelSelection.empty() || tf->modelSelection != newModel || tf->useGPU != newUseGpu ||
       tf->numThreads != newNumThreads) {
     tf->numThreads = newNumThreads;
     tf->modelSelection = newModel;
@@ -93,7 +114,7 @@ static void filter_update(void *data, obs_data_t *settings)
     } else {
       tf->model.reset(new ModelBCHW);
     }
-    tf->useGPU = USEGPU_CPU;
+    tf->useGPU = newUseGpu;
     createOrtSession(tf);
   }
 
@@ -185,9 +206,7 @@ static void filter_video_tick(void *data, float seconds)
       return;
     }
 
-    // TODO add high pass filter from original image to output image
-    // to reduce the blur effect of the small network output
-
+    // convert to RGBA
     cv::cvtColor(outputImage, tf->outputBGRA, cv::COLOR_BGR2RGBA);
   }
 }
@@ -226,19 +245,19 @@ static void filter_video_render(void *data, gs_effect_t *_effect)
 
   gs_eparam_t *blendimage = gs_effect_get_param_by_name(tf->blendEffect, "blendimage");
   gs_eparam_t *blendFactor = gs_effect_get_param_by_name(tf->blendEffect, "blendFactor");
+  gs_eparam_t *xOffset = gs_effect_get_param_by_name(tf->blendEffect, "xOffset");
+  gs_eparam_t *yOffset = gs_effect_get_param_by_name(tf->blendEffect, "yOffset");
 
   gs_effect_set_texture(blendimage, outputTexture);
   gs_effect_set_float(blendFactor, tf->blendFactor);
+  gs_effect_set_float(xOffset, 1.0f / float(width));
+  gs_effect_set_float(yOffset, 1.0f / float(height));
 
   // Render texture
   gs_blend_state_push();
   gs_reset_blend_state();
 
   obs_source_process_filter_tech_end(tf->source, tf->blendEffect, 0, 0, "Draw");
-
-  // while (gs_effect_loop(tf->blendEffect, "Draw")) {
-  //   obs_source_draw(outputTexture, 0, 0, width, height, false);
-  // }
 
   gs_blend_state_pop();
 
