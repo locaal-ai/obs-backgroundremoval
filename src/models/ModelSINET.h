53abbac1899f334726ffe8453b8d5d398779b600
Roy Shilkrot <roy.shil@gmail.com>
2023-06-07 11:30:48 -0400

    fix sinet inference

--- src/models/ModelSINET.h
+++ src/models/ModelSINET.h
@@ -10,10 +10,26 @@ class ModelSINET : public ModelBCHW {
 
   virtual void prepareInputToNetwork(cv::Mat &resizedImage, cv::Mat &preprocessedImage)
   {
-    cv::subtract(resizedImage, cv::Scalar(102.890434, 111.25247, 126.91212), resizedImage);
-    cv::multiply(resizedImage, cv::Scalar(1.0 / 62.93292, 1.0 / 62.82138, 1.0 / 66.355705) / 255.0,
-                 resizedImage);
+    resizedImage = (resizedImage - cv::Scalar(102.890434, 111.25247, 126.91212)) /
+      cv::Scalar(62.93292 * 255.0, 62.82138 * 255.0, 66.355705 * 255.0);
     hwc_to_chw(resizedImage, preprocessedImage);
+  }
+
+  virtual cv::Mat getNetworkOutput(const std::vector<std::vector<int64_t>> &outputDims,
+                                   std::vector<std::vector<float>> &outputTensorValues)
+  {
+    UNUSED_PARAMETER(outputDims);
+    return cv::Mat(320, 320, CV_32FC2, outputTensorValues[0].data());
+  }
+
+  virtual void postprocessOutput(cv::Mat &outputImage)
+  {
+    cv::Mat outputTransposed;
+    chw_to_hwc_32f(outputImage, outputTransposed);
+    // take 2nd channel
+    std::vector<cv::Mat> outputImageSplit;
+    cv::split(outputTransposed, outputImageSplit);
+    outputImage = outputImageSplit[1];
   }
 };
 
