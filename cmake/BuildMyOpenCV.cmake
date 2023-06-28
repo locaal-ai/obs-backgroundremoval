86107fe80191607282ce618e24dfeabb1ecd03ef
Kaito Udagawa <umireon@gmail.com>
2023-06-28 11:09:11 +0900

    Update BuildMyOpenCV.cmake

--- cmake/BuildMyOpenCV.cmake
+++ cmake/BuildMyOpenCV.cmake
@@ -11,17 +11,12 @@ set(OPENCV_MD5
 string(REPLACE ";" "$<SEMICOLON>" CMAKE_OSX_ARCHITECTURES_ "${CMAKE_OSX_ARCHITECTURES}")
 
 if(MSVC)
-  find_program(OpenCV_CCACHE_EXE ccache)
   if(${CMAKE_GENERATOR_PLATFORM} STREQUAL x64
      AND ${MSVC_VERSION} GREATER_EQUAL 1910
      AND ${MSVC_VERSION} LESS_EQUAL 1939)
     set(OpenCV_LIB_PATH x64/vc17/staticlib)
     set(OpenCV_LIB_PATH_3RD x64/vc17/staticlib)
     set(OpenCV_LIB_SUFFIX 470)
-    # if(OpenCV_CCACHE_EXE) set(OpenCV_INSTALL_CCACHE ${CMAKE_COMMAND} -E copy ${OpenCV_CCACHE_EXE} <BINARY_DIR>/cl.exe)
-    # set(OpenCV_PLATFORM_CMAKE_ARGS
-    # -DCMAKE_VS_GLOBALS=CLToolExe=cl.exe$<SEMICOLON>CLToolPath=<BINARY_DIR>$<SEMICOLON>TrackFileAccess=false$<SEMICOLON>UseMultiToolTask=true$<SEMICOLON>DebugInformationFormat=OldStyle
-    # ) endif()
   else()
     message(FATAL_ERROR "Unsupported MSVC!")
   endif()
