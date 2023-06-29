e48d9c9e7f1d89c86f68b69268c1b0ead8f89a23
Kaito Udagawa <umireon@gmail.com>
2023-06-29 22:26:34 +0900

    Update compiler_common.cmake

--- cmake/common/compiler_common.cmake
+++ cmake/common/compiler_common.cmake
@@ -47,6 +47,7 @@ set(_obs_clang_c_options
     -Wno-strict-prototypes
     -Wno-trigraphs
     -Wno-unknown-pragmas
+    -Wno-unused-command-line-argument
     -Wno-unused-function
     -Wno-unused-label
     -Wnon-literal-null-conversion
@@ -61,8 +62,7 @@ set(_obs_clang_c_options
     -Wunused-parameter
     -Wunused-value
     -Wunused-variable
-    -Wvla
-    -Wno-unused-command-line-argument)
+    -Wvla)
 
 # clang options for C++
 set(_obs_clang_cxx_options
