65cd74fd14becf71f3156740035c5d10718f57b6
Kaito Udagawa <umireon@gmail.com>
2023-07-01 00:47:32 +0900

    Update background-filter.cpp

--- src/background-filter.cpp
+++ src/background-filter.cpp
@@ -43,6 +43,8 @@ struct background_removal_filter : public filter_data {
 
 	gs_effect_t *effect;
 	gs_effect_t *kawaseBlurEffect;
+
+	QNetworkAccessManager qnam;
 };
 
 const char *background_filter_getname(void *unused)
