39af5c09ddabd86cfc8f616e944ace5c6d293394
Kaito Udagawa <umireon@gmail.com>
2023-10-04 23:10:48 +0900

    First SvelteKit

---
+++ pages/svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			fallback: "index.html",
+		})
+	}
+};
+
+export default config;
